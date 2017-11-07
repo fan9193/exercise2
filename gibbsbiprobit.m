2c96dd61d8fa8bcc3ddb1efdce52e2ed0ed7b064
fan9193 <33267644+fan9193@users.noreply.github.com>
2017-11-06 19:53:30 -0800

    Update gibbsbiprobit.m

--- gibbsbiprobit.m
+++ gibbsbiprobit.m
@@ -21,23 +21,17 @@ function [out1,out2] = gibbsbiprobit(k,Y,X,beta_0,B_0)
    betadraw = (X'*X)^(-1)*X'*Y; %initial draw of beta
 
    for t = 1:k
-       for i = 1:n
-           a=X(i,:)*betadraw;
-           
-           % truncated normal
-           pd = makedist('normal','mu',a,'sigma',1);
-           t1=truncate(pd,0,inf);
-           t2 = truncate(pd,-inf,0);
-           temp = random(t1)*Y(i)+random(t2)*(1-Y(i));
-           temp1 = [temp1,temp];
-       end
-       Z = (temp1(2:n+1))';
+       % change a different truncate function in matlab
+       % posterial distribution of Z
+       a = X*betadraw;
+       Z = trandn(-a,inf*ones(n,1)).^Y .* trandn(-inf*ones(n,1),-a).^(1-Y); %n*1
+       
        
        % posterial distribution ~ N(beta,B)
        beta = (inv(B_0) + X'*X)\(inv(B_0)*beta_0 + X'*Z);
        B = inv(inv(B_0)+ X'*X);
        betadraw = (mvnrnd(beta,B))';
-       beta_store(t,:) = betadraw;
+       beta_store(t,:) = betadraw';
    end
    out1 = beta_store;
    out2 = B;
